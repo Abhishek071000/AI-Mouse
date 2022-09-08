e3e9c1cedc6cfeae88d479a7cd528af4bd0e77aa
Abhishek071000 <abhishek.goswami9013887401@gmail.com>
2022-09-08 10:15:41 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,20 @@
+
+smoothening = 7  #random value
+######################
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+detector = htm.handDetector(maxHands=1)
+wScr, hScr = autopy.screen.size()
+
+# print(wScr, hScr)
+
+while True:
+    # Step1: Find the landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
